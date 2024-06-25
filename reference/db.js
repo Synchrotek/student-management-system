21e465159999ba66fd180cba3c718f8054f653aa
adarshdebata <adarshdebata00@gmail.com>
2024-06-25 12:32:39 +0530

    minor modification

--- reference/db.js
+++ reference/db.js
@@ -32,3 +32,27 @@ PGUSER = 'Student-management-system_owner'
 PGPASSWORD = 'jNo9xis7DkUc'
 ENDPOINT_ID = 'ep-sweet-math-a1pjc4md-pooler'
  */
+
+// // models/db.js
+// const { Sequelize } = require('sequelize');
+
+// // Database credentials from Neon Console
+// const sequelize = new Sequelize('Student-management-system', 'Student-management-system_owner', 'jNo9xis7DkUc', {
+//   host: 'ep-sweet-math-a1pjc4md-pooler.ap-southeast-1.aws.neon.tech',
+//   dialect: 'postgres',
+//   dialectOptions: {
+//     ssl: {
+//       require: true, 
+//       rejectUnauthorized: false 
+//     }
+//   }
+// });
+// sequelize.authenticate()
+//   .then(() => {
+//     console.log('Database connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+
+// module.exports = sequelize;
